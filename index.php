c0f984064b8a41a916ca4f161b4b05e2b0d2822d
wahyudi-unand <wahyudi.unand@gmail.com>
2020-07-21 22:35:05 +0700

    membuat file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Folder latihan Git</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nulla, expedita nemo soluta ad consectetur, officiis, corrupti repellendus eveniet dolorem aperiam a nostrum animi unde totam vero earum distinctio incidunt?</p>
+</body>
+
+</html>
